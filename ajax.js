41d3d1f18a1b9a19677fe478dde0629e41d2e5be
jeezalro <jesus.alberto.lopez@outlook.com>
2014-08-31 23:33:21 -0600

    cambies

--- ajax.js
+++ ajax.js
@@ -1,74 +1,59 @@
-$(function (){
-	$.ajax('logos_footer.html #maestros',function(html){
-		$('footer').prepend(html);
-	})
-	$.get('usuario.json', function(info){
-		var avatar = new Image();
-		avatar.src = info.avatar;
-		avatar.title = info.nombre+' '+info.apellido;
-		$('#avatar').html(avatar).append(avatar.title)
-	});
+$(function(){
+
+	
+
 });
-var base_url = "http://query.yahooapis.com/v1/public/yql?";
+var url_base='https://query.yahooapis.com/v1/public/yql?q=';
 
-function obtenerGeoInfo(lat,lon){
-	console.log(lat,lon);
-	var query = 'select * from geo.placefinder where text="'+lat+','+lon+'" AND gflags ="R"';
+function obtenerUbicacion(lat,lon){
+		console.log(lat+"y"+lon); 
+		var query= 'select * from geo.placefinder where text="'+lat+', '+lon+'" and gflags="R"';
+		console.log(query);
+		query= encodeURIComponent(query);
+		console.log(query);
+		$.ajax({
+			url: url_base+query,
+			dataType: 'jsonp',
+			jsonpCallback: 'procesar',
+			data:{
+				format:'json'
+			}
+		})
+	};	
+function procesar(datos){
+	console.log(datos)
+	var ubicacion= datos.query.results.Result;
+	var pais = ubicacion.country;
+	var estado = ubicacion.county
+
+	$('#clima').append('<br>'+pais+' '+estado);
+	obtenerClima(ubicacion);
+};
+function obtenerClima(ubicacion){
+	console.log(ubicacion)
+	var idclima = ubicacion.woeid;
+	console.log(idclima);
+	var query= 'select * from weather.forecast where woeid="'+idclima+'" and u="c"';
 	console.log(query);
-
-	query = encodeURIComponent(query);
-
+	query= encodeURIComponent(query);
+	console.log(query)
 	$.ajax({
-		url: base_url+'q='+query,
-		dataType: 'jsonp',
-		jsonpCallback: 'procesarGeoInfo',
-		data:{
-			format: 'json'
-		}
-
-	});
-}
-
-function obtenerClima(tiempo){
-	console.log(tiempo);
-	var query = 'select * from weather.forecast where woeid='+tiempo+' and u="c"';
-	console.log(query);
-
-	query = encodeURIComponent(query);
-
-	$.ajax({
-		url: base_url+'q='+query,
+		url: url_base+query,
 		dataType: 'jsonp',
 		jsonpCallback: 'procesarClima',
 		data:{
 			format: 'json'
 		}
 
-	});
-}
-
-
-function procesarGeoInfo(datos){
-	console.log(datos);
-	var resulador = datos.query.results.Result;
-	var barrio = resulador.county;
-	var ciudad = resulador.city;
-	var pasi = resulador.country;
-	var tiempo = resulador.woeid;
-
-	$('#geo').prepend('<p>'+pasi+'<br>'+barrio+'<br>'+ciudad+'</p>')
-	obtenerClima(tiempo);
-}
-function procesarClima(tiempo){
-console.log(tiempo)
-	var clima = tiempo.query.results.channel;
-	var temp = clima.item.condition.temp;
-	var unit = clima.units.temperature;
-	var cond = clima.item.condition.code;
-	var img = new Image();
-	img.src= 'http://l.yimg.com/a/i/us/we/52/'+cond+'.gif';
-
-	$('#clima')
-	.prepend('<p>'+temp+' '+unit+'°</p>')
-}
-
+	})
+};
+function procesarClima(datos){
+	console.log(datos)
+	var infoC= datos.query.results.channel.item;
+	var temp= infoC.condition.temp;
+	var code = infoC.condition.code;
+	var img = new Image()
+	 img.src= 'http://l.yimg.com/a/i/us/we/52/'+code+'.gif'
+	console.log(temp+" "+code)
+	$('#clima').prepend(temp+"°").prepend(img)
+};
--- geo.js
+++ geo.js
@@ -1,20 +1,22 @@
-$(function(){
-	var geo = navigator.geolocation,
-		opciones= {};
+$(function (){
+var geo = navigator.geolocation;
+console.log(geo);
+var cosa={}
+function mostrar(posicion){
+	console.log(posicion);
+	var lat = posicion.coords.latitude;
+	var lon = posicion.coords.longitude;
+	console.log(lat+" "+lon)
+	var html = '<iframe src="https://www.google.com/maps/embed?pb=!1m13!1m11!1m3!1d10983.908985226486!2d'+lon+'!3d'+lat+'!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2ses!4v1409535191066" width="100%" height="200" frameborder="0" style="border:0"></iframe>'
+	$('#geo').append(html);
 
-	function geo_error(){
-		console.log('HHmm no puedo saber tu ubicacions')
-	}
-	function geo_exito(posicion){
-		console.log(posicion)
-		var lat = posicion.coords.latitude;
-		var lon = posicion.coords.longitude;
-		var mapa = new Image();
-		mapa.src="http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=200x200&sensor=false&center="+lat+","+lon;
-		$('#geo').prepend(mapa);
+	obtenerUbicacion(lat,lon);
+}
+function  error(){
+	console.log("error");
 
-		obtenerGeoInfo(lat,lon);
-	}
 
-	geo.getCurrentPosition(geo_exito,geo_error,opciones);
-})
+}
+ geo.getCurrentPosition(mostrar,error,cosa)
+});
+
